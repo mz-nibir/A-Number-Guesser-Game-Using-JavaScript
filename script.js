965c781d983437eb4e856cdf2f3673317224be5b
Nibir <mz.nibir@gmail.com>
2021-01-23 00:10:33 +0600

    update condition

--- script.js
+++ script.js
@@ -1,49 +1,57 @@
 let result = document.querySelector("#clickBtn");
-let randomNumber = Math.floor(Math.random() * 10 + 1); 
+let randomNumber = Math.floor(Math.random() * 10 + 1);
 
 //add eventListener
 
 result.addEventListener('click', (e) => {
 
- for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
 
-     console.log(randomNumber);
-    let correcr_ans = prompt("Enter the Guess Number: ");
+        console.log(randomNumber);
+        let correcr_ans = prompt("Enter the Guess Number: ");
 
-    if(correcr_ans == ""){
-        alert("Invalid Input!!");
+        if (correcr_ans == "") {
+            alert("Invalid Input!!");
 
-    }else if(correcr_ans == randomNumber){
-        alert("CONGRATULATIONS!!! You Win ");
-        var count = 100; 
-        break;
-        
-    }else if(correcr_ans > randomNumber && correcr_ans < 11){ 	 
-            alert("Wrong! Correct Number is less than your input!!! "); 
+        } else if (correcr_ans == randomNumber) {
+            alert("CONGRATULATIONS!!! You Win ");
+            var count = 100;
+            break;
 
-    }else if(correcr_ans < randomNumber && correcr_ans > 0) { 
-        alert("Wrong! Correct Number is greater than your input!!!") ;
+        } else if (correcr_ans > randomNumber && correcr_ans < 11) {
+            if (i == 2) {
+                alert("Wrong!  Over your Chance!!!");
+                break;
+            }
+            alert("Wrong! Correct Number is less than your input!!! ");
+
+        } else if (correcr_ans < randomNumber && correcr_ans > 0) {
+            if (i == 2) {
+                alert("Wrong!  Over your Chance!!!");
+                break;
+            }
+            alert("Wrong! Correct Number is greater than your input!!!");
+        }
+        else {
+            alert("please input between 1 to 10 !");
+
+        }
+
     }
-    else { 
-        alert("please input between 1 to 10 !");
 
+
+
+
+    if (count != 100) {
+        alert(`You lose the game! The Number was ${randomNumber}`);
     }
 
-    }
 
-    
-     
-     
- if (count != 100){
-    alert(`You lose the game! The Number was ${randomNumber}` );
- }
- 
 
- 
 
-    
 
- 
+
+
 
 
 
