613940ce0eb7d6b05ea4a714adb131a2bdd79a39
Nibir <mz.nibir@gmail.com>
2021-01-22 22:37:55 +0600

    update done

--- script.js
+++ script.js
@@ -4,26 +4,46 @@ let randomNumber = Math.floor(Math.random() * 10 + 1);
 //add eventListener
 
 result.addEventListener('click', (e) => {
-    console.log(randomNumber);
 
-    
+ for (let i = 0; i < 3; i++) {
 
+     console.log(randomNumber);
     let correcr_ans = prompt("Enter the Guess Number: ");
 
     if(correcr_ans == ""){
         alert("Invalid Input!!");
-    }else{
-        if(correcr_ans == randomNumber){	 
-            alert("CONGRATULATIONS!!! You Win "); 
-            
-        }else if(correcr_ans > randomNumber) {	 
-            alert("Correct answer is smaller! input lower number"); 
-        }
-        else { 
-            alert("Correct answer is greater!  input big number") ;
-        }
+
+    }else if(correcr_ans == randomNumber){
+        alert("CONGRATULATIONS!!! You Win ");
+        var count = 100; 
+        break;
+        
+    }else if(correcr_ans > randomNumber && correcr_ans < 11){ 	 
+            alert("Wrong! Correct Number is less than your input!!! "); 
+
+    }else if(correcr_ans < randomNumber && correcr_ans > 0) { 
+        alert("Wrong! Correct Number is greater than your input!!!") ;
+    }
+    else { 
+        alert("please input between 1 to 10 !");
 
     }
+
+    }
+
+    
+     
+     
+ if (count != 100){
+    alert(`You lose the game! The Number was ${randomNumber}` );
+ }
+ 
+
+ 
+
+    
+
+ 
 
 
 
